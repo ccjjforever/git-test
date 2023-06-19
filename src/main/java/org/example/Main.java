863f5d1e7456b6422e00555f69728f230677e654
mabang <love@love>
2023-06-19 18:56:54 +0800

    hf 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello hot-fix");
         System.out.println("Hello hot-fix");
         System.out.println("Hello hot-fix");
+        System.out.println("hot-fix test");
     }
 }
